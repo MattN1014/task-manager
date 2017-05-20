8d93bab46dfed24bc60b0b18e5b58bdd09a125fc
Matt <matty_noye@live.com>
2017-05-21 00:50:39 +0100

    Index file added with basic markup

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="robots" content="all,follow">
+        <meta name="googlebot" content="index,follow,snippet,archive">
+
+        <title>Freelance Web Designer & Web Developer, Darlington UK | MWN Design</title>
+
+        <link rel="stylesheet" href="assets/css/bootstrap.css">
+        <link rel="stylesheet" href="assets/css/font-awesome.css">
+        <link rel="stylesheet" href="assets/css/custom.css">
+
+    </head>
+    <body>
+
+
+    	<!-- load JS -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script src="assets/js/bootstrap.js"></script>
+        <!--<script src="assets/js/script.js"></script> -->
+    </body>
+</html>
