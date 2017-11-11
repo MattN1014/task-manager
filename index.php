269ddf039329ef00e8781306fb776039933556b4
Matt <matt@mwn-design.co.uk>
2017-11-11 04:31:41 +0000

    Index file has been changed, this now has the create task form added in and the header and footer parts of the page have now been added to their respective files

--- index.php
+++ index.php
@@ -1,25 +1,30 @@
-<!DOCTYPE HTML>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="robots" content="all,follow">
-        <meta name="googlebot" content="index,follow,snippet,archive">
+<?php 
+    $pageTitle = "Create Task";
+    include'includes/header.php'; 
+?>
 
-        <title>Freelance Web Designer & Web Developer, Darlington UK | MWN Design</title>
+    <div class="container-fluid">
+        <section class="col-lg-6 col-sm-12">
+            <h3 class="text-primary">Create a new task</h3><hr>
+            <form action="" method="POST">
+                <div class="form-group">
+                    <label class="col-lg-2 control-label">Name</label>
+                    <div class="col-lg-10">
+                        <input name="name" class="form-control" id="name" type="text">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-lg-12 control-label">Description</label>
+                    <div class="col-lg-10">
+                        <textarea class="form-control" rows="3" name="description" id="description"></textarea>
+                    </div>
+                </div>
+                <button type="submit" name="createBtn" class="btn btn-success pull-right">
+                    Create Task <i class="fa fa-plus"></i>
+                </button>
+            </form>
+        </section>
+    </div>
+        
 
-        <link rel="stylesheet" href="assets/css/bootstrap.css">
-        <link rel="stylesheet" href="assets/css/font-awesome.css">
-        <link rel="stylesheet" href="assets/css/custom.css">
-
-    </head>
-    <body>
-
-
-    	<!-- load JS -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-        <script src="assets/js/bootstrap.js"></script>
-        <!--<script src="assets/js/script.js"></script> -->
-    </body>
-</html>
+<?php include'includes/footer.php'; ?>
