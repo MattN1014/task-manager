a1c9aefbe11b66ad6825072465e21bcc65458e77
Matt <matt@mwn-design.co.uk>
2017-11-12 02:21:56 +0000

    Function added to clear the form once data has been submited

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,7 @@ $(document).ready(function(){
 			data: formData,
 			success: function(data){
 				$('#ajax_msg').css("display", "block").delay(3000).slideUp(300).html(data);
+				document.getElementById("create-task").reset();
 			}
 		});
 	});
