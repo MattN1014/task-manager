69a3598121011d3ea84f286d1cd3c39edafe11a6
Matt <matt@mwn-design.co.uk>
2017-11-12 04:10:36 +0000

    Finished creating all of the scripting for the editable regions of the tasks.

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,3 +19,65 @@ $(document).ready(function(){
 	$('#task-list').load('read.php');
 
 });
+
+//Creates editable element, used for editing tasks
+function makeElementEditable(div) {
+	div.style.border = "1px solid green";
+	div.style.padding = "5px";
+	div.style.background = "white";
+	div.contentEditable = true;
+}
+
+//enabled update of task name
+function updateTaskName(target, taskId){
+	var data = target.textContent;
+	target.style.border = "none";
+	target.style.padding = "0px";
+	target.style.background = "#ececec";
+	target.contentEditable = false;
+
+	$.ajax({
+		url: 'update.php', 
+		method: 'POST',
+		data: {name: data, id:taskId},
+		success: function(data){
+			$('#ajax_msg').css("display", "block").delay(3000).slideUp(300).html(data);
+		}
+	});
+}
+
+//enable update of task description
+function updateTaskdescription(target, taskId){
+	var data = target.textContent;
+	target.style.border = "none";
+	target.style.padding = "0px";
+	target.style.background = "#ececec";
+	target.contentEditable = false;
+
+	$.ajax({
+		url: 'update.php', 
+		method: 'POST',
+		data: {description: data, id:taskId},
+		success: function(data){
+			$('#ajax_msg').css("display", "block").delay(3000).slideUp(300).html(data);
+		}
+	});
+}
+
+//enable update of task status
+function updateTaskstatus(target, taskId){
+	var data = target.textContent;
+	target.style.border = "none";
+	target.style.padding = "0px";
+	target.style.background = "#ececec";
+	target.contentEditable = false;
+
+	$.ajax({
+		url: 'update.php', 
+		method: 'POST',
+		data: {status: data, id:taskId},
+		success: function(data){
+			$('#ajax_msg').css("display", "block").delay(3000).slideUp(300).html(data);
+		}
+	});
+}
