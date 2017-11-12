5c90d271374288c88897f8329b60396bbf4e46b2
Matt <matt@mwn-design.co.uk>
2017-11-12 02:07:01 +0000

    change the naming of some js files

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,8 @@
 <!-- load JS -->
         <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
         <script src="assets/js/bootstrap.js"></script>
-        <script src="assets/js/material.min.js"></script>
-        <script src="assets/js/ripples.min.js"></script>
+        <script src="assets/js/material.js"></script>
+        <script src="assets/js/ripples.js"></script>
         <script src="assets/js/script.js"></script>
 
         <script> $.material.init();</script>
