4f784ca38cda46aa27c5ebfce1538614f0b01216
Matt <matt@mwn-design.co.uk>
2017-11-27 02:49:22 +0000

    Changed some settings within the SQL file to allow for longer task names & also corrected a spelling mistake on the task status default state

--- SQL_file/taskmanager.sql
+++ SQL_file/taskmanager.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 23, 2017 at 10:37 PM
+-- Generation Time: Nov 27, 2017 at 02:46 AM
 -- Server version: 5.7.19
 -- PHP Version: 5.6.31
 
@@ -31,20 +31,21 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `tasks`;
 CREATE TABLE IF NOT EXISTS `tasks` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` varchar(35) NOT NULL,
-  `description` varchar(255) NOT NULL,
-  `status` varchar(25) DEFAULT 'Not Complated',
+  `name` varchar(125) NOT NULL,
+  `description` varchar(510) NOT NULL,
+  `status` varchar(25) DEFAULT 'Not Completed',
   `created_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tasks`
 --
 
 INSERT INTO `tasks` (`id`, `name`, `description`, `status`, `created_at`) VALUES
-(1, ' Testing the new script feature', 'This new script feature allows direct editing of the data from the DB in the table.Â  YAY', 'Completed', '2017-11-12 02:02:12');
+(1, 'Hello there im a new task', 'Im the first new task since making some optimisation changes', ' Completed ', '2017-11-27 02:04:05'),
+(2, 'Second task ', 'Im the second new task since optimisation', 'Not Completed', '2017-11-27 02:04:17');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
